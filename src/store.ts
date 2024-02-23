5af1c1b0bac3d70e404fa4eeb6f3a99520f617f5
Jens <jens@touchtribe.nl>
2024-02-23 11:11:25 +0100

    Fixed duration race condition

--- src/store.ts
+++ src/store.ts
@@ -3,40 +3,44 @@ import { create } from 'zustand'
 
 type PageTransitionState = {
   isTransitioning: boolean
+  currentDuration: number | null
   path: string | null
 }
 
 type PageTransitionStore = {
   pageTransitionState: PageTransitionState
   setPageTransitionState: (state: PageTransitionState) => void
-  duration: number | null
-  setDuration: (duration: number | null) => void
+  nextDuration: number | null
+  setNextDuration: (duration: number | null) => void
   startTransition: (href: string, router: AppRouterInstance) => void
 }
 
 export const usePageTransitionStore = create<PageTransitionStore>((set, get) => ({
   pageTransitionState: {
     isTransitioning: false,
+    currentDuration: null,
     path: null,
   },
   setPageTransitionState: (pageTransitionState) =>
     set((state) => ({ ...state, pageTransitionState })),
-  duration: null,
-  setDuration: (duration) => {
-    const existingDuration = get().duration
+  nextDuration: null,
+  setNextDuration: (duration) => {
+    const existingNextDuration = get().nextDuration
 
-    if (!duration || !existingDuration) {
+    if (!duration || !existingNextDuration) {
       set((state) => ({ ...state, duration }))
       return
     }
 
-    set((state) => ({ ...state, duration: Math.max(duration, existingDuration) }))
+    set((state) => ({ ...state, duration: Math.max(duration, existingNextDuration) }))
   },
   startTransition: (href, router) => {
     set((state) => ({
       ...state,
+      nextDuration: null,
       pageTransitionState: {
         isTransitioning: true,
+        currentDuration: state.nextDuration,
         path: href,
       },
     }))
